fb53ed257bd8e8f692e60531d8ae2a783a9b0ec2
Sergio Maciel <sergiobmaciel@gmail.com>
2014-01-13 22:43:07 -0200

    Commmi 4

--- ControleDeCasa/src/br/maciel/controledecasa/PrincipalActivity.java
+++ ControleDeCasa/src/br/maciel/controledecasa/PrincipalActivity.java
@@ -5,15 +5,12 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import br.maciel.controledecasa.dao.DataBaseConnector;
-import br.maciel.controledecasa.model.Configuracao;
 import br.maciel.controledecasa.utils.ClienteHttpGet;
 
 public class PrincipalActivity extends Activity {
